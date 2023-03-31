7eb577f101957a53b60bd821eab98db0a2df1e1b
EvgenRud <rudnik.evgen@gmail.com>
2023-03-31 17:57:21 +0400

    Функция для расчета среднего арифметического

--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
+# Функция для расчета среднего арифметического
+def average(numbers):
+    value = sum(numbers) / len(numbers)
+    print(f'Функцию вызвали с параметром {value}')
+    return value
+
+my_list = [4,7,2,5,4]
+average(my_list)
+
+
+
+
 
